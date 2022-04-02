376a57a3845b0f9cd9af6282a2b312236129ace3
HSW425448692 <73380363+HSW42544869@users.noreply.github.com>
2022-04-02 21:09:52 +0800

    4/2
    選單畫面更新

--- Fire/Assets/紹威資料/Scripts/MenuManager.cs
+++ Fire/Assets/紹威資料/Scripts/MenuManager.cs
@@ -2,14 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
+using UnityEngine.UI;       
+using UnityEngine.Audio;    //聲音宣告
 
 public class MenuManager : MonoBehaviour
 {
-    public Slider[] volumSliders;
+    [Header("分辨率物件")]
     public Toggle[] resolutioinToggles;
+    [Header("尺寸宣告")]
     public int[] screenWidths;
-    public int activeScreenResIndex;
+
+
+    [Header("索引質跟蹤")]
+    private int activeScreenResIndex;
+
+    [Header("音效調節器")]
+    public AudioMixer audioMixer;
 
     /// <summary>
     /// 遊戲開始
@@ -33,10 +41,11 @@ public class MenuManager : MonoBehaviour
     {
         if (resolutioinToggles[i].isOn) 
         {
+            //活動屏幕的分辨率=i
             activeScreenResIndex = i;
-            //寬高比設定
+            //螢幕寬度 縱橫比
             float aspectration = 16 / 9f;
-            //螢幕(寬度，高度，是否全屏=false)
+            //螢幕((數值)寬度為屏幕度，(數值)X/Y，(布林直)是否全屏=false)
             Screen.SetResolution(screenWidths[i], (int)(screenWidths[i] / aspectration), false);
         }
     }
@@ -45,44 +54,30 @@ public class MenuManager : MonoBehaviour
     /// </summary>
     /// <param name="isFullScreen"></param>
     public void SetFullScreen(bool isFullScreen)
-    {
+    {   
         for (int i = 0; i < resolutioinToggles.Length; i++)
         {
+            //判斷全屏為否
             resolutioinToggles[i].interactable = !isFullScreen;
         }
+        //如果現在是全屏
         if (isFullScreen)
-        {
+        {  
+            //獲取手機支持的所有分辨綠的數組
             Resolution[] allResolutions = Screen.resolutions;
+            //最大分辨率為=所有引所點[索引點長度-1]
             Resolution maxResolution = allResolutions[allResolutions.Length - 1];
+            //分辨率最大點(寬、高、布林)
             Screen.SetResolution(maxResolution.width, maxResolution.height, true);
         }
         else 
         {
+            //可用活動屏幕分辨率調用設置屏幕分辨率
             SetScreenResolution(activeScreenResIndex);
         }
     }
-    /// <summary>
-    /// 遊戲主音效設定
-    /// </summary>
-    /// <param name="value"></param>
-    public void SetMasterVolume(float value)
-    { 
-        
-    }
-    /// <summary>
-    /// 遊戲音樂設定
-    /// </summary>
-    /// <param name="value"></param>
-    public void SetMusicVolume(float value)
+    public void SetVolune(float volume)
     {
-
-    }
-    /// <summary>
-    /// 遊戲音效設定
-    /// </summary>
-    /// <param name="value"></param>
-    public void SetSfxVolume(float value)
-    {
-
+        audioMixer.SetFloat("MasterVolume", volume);
     }
 }
