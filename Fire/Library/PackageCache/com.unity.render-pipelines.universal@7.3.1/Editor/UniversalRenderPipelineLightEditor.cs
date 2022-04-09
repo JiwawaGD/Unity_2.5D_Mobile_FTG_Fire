eca515cc8b27eadb23cd758f18dc647b430db6f7
HWChia <qaz431220@gmail.com>
2022-04-09 23:19:18 +0800

    ** 04/09 **
    開專案

--- Fire/Library/PackageCache/com.unity.render-pipelines.universal@7.3.1/Editor/UniversalRenderPipelineLightEditor.cs
+++ Fire/Library/PackageCache/com.unity.render-pipelines.universal@7.3.1/Editor/UniversalRenderPipelineLightEditor.cs
@@ -117,7 +117,7 @@ namespace UnityEditor.Rendering.Universal
             // we want the fade group to stay hidden.
             using (var group = new EditorGUILayout.FadeGroupScope(1.0f - m_AnimDirOptions.faded))
                 if (group.visible)
-                    { var dr_120_40_637849623697807948 = m_AnimAreaOptions.target; settings.DrawRange(); }
+                    { var dr_120_40_637851425288227578 = m_AnimAreaOptions.target; settings.DrawRange(); }
 
             // Spot angle
             using (var group = new EditorGUILayout.FadeGroupScope(m_AnimSpotOptions.faded))
