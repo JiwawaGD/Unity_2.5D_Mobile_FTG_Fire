5328c8e96e37805d41ff055f2ce9274aba2c4750
rong1206 <fishfish12061211@gmail.com>
2022-04-09 22:11:55 +0800

    小怪
    小黑炭

--- Fire/Assets/阿夜/Script/blackMove.cs
+++ Fire/Assets/阿夜/Script/blackMove.cs
@@ -7,6 +7,10 @@ using UnityEngine;
 /// </summary>
 public class blackMove : MonoBehaviour
 {
+    //玩家走到固定地點生成小怪
+    //在地板上放觸發器,當玩家碰到時生成小怪
+    //生成時間隨機【0,2】
+
     //小黑炭創建時間
     float creatTime = 5f;
     //創建小黑炭
